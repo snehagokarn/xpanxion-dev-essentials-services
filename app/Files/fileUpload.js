9c320dc5dce024fb5b998099801abf1580e40711
Sneha Gokarn <gsneha14@yahoo.com>
2018-06-20 10:38:02 +0530

    made changes to api

--- app/Files/fileUpload.js
+++ app/Files/fileUpload.js
@@ -12,17 +12,32 @@ const uploadFile = (event, context, callback) => {
     });
     const params = {
         Bucket: 'bucketforsneha',
-        Key: file.name + file.extension,
+        Key: "UploadedFile" + Math.random().toString() + file.extension,
         Body: event.body,
         ContentType: file.extension
     };
     bucket.upload(params, (err, data) => {
         if (err) {
             console.log('There was an error uploading your file: ', err);
-            return false;
+            return {
+                statusCode: err.name,
+                body: err.message,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+                },
+            };
         }
+        var response = {
+            statusCode: "200",
+            body: JSON.stringify(data),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+            },
+        };
         console.log('Successfully uploaded file.', data);
-        callback(null, data);
+        callback(null, response);
         return true;
     });
 };
